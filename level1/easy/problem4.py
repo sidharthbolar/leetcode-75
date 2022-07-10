667e5650955edfede85429edb888a39e123f46ad
Sidharth Bolar <sidharth.bolar89@gmail.com>
2022-07-10 15:39:25 +1000

    Create problem4.py

---
+++ level1/easy/problem4.py
@@ -0,0 +1,23 @@
+'''
+isSubsequence
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+Example 1:
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+Input: s = "axc", t = "ahbgdc"
+Output: false
+'''
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if len(s) == 0:
+            return True
+        if len(t) == 0:
+            return False
+        for i, t_c in enumerate(t):
+            print(i,t_c)
+            if s[0] == t_c:
+                return self.isSubsequence(s[1:], t[i+1:])
+        return False
