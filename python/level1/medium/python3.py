4a7a5852b82f6f2d2fb589937a8ac77f95e51ca9
Sidharth Bolar <sidharth.bolar89@gmail.com>
2022-07-19 23:53:38 +1000

    pym3

---
+++ python/level1/medium/python3.py
@@ -0,0 +1,11 @@
+'''
+Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, color = 2
+Output: [[2,2,2],[2,2,0],[2,0,1]]
+Explanation: From the center of the image with position (sr, sc) = (1, 1) (i.e., the red pixel), all pixels connected by a path of the same color as the starting pixel (i.e., the blue pixels) are colored with the new color.
+Note the bottom corner is not colored 2, because it is not 4-directionally connected to the starting pixel.
+Input: image = [[0,0,0],[0,0,0]], sr = 0, sc = 0, color = 0
+Output: [[0,0,0],[0,0,0]]
+Explanation: The starting pixel is already colored 0, so no changes are made to the image.
+'''
+
+#To be solved
