6cc064cd9f4dfe483e3cfab5f2391c25ea4356ed
Sidharth Bolar <sidharth.bolar89@gmail.com>
2022-07-23 07:04:17 +1000

    pye15

--- python/level1/easy/python15.py
+++ python/level1/easy/python15.py
@@ -6,4 +6,21 @@ The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonac
 F(0) = 0, F(1) = 1
 F(n) = F(n - 1) + F(n - 2), for n > 1.
 Given n, calculate F(n).
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+
+Input: n = 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
 '''
+
+class Solution:
+    def fib(self, n: int) -> int:
+        def fibaux(self, n):
+            if (n == 0) | (n == 1):
+                return n
+            else:
+                return fibaux(self, n - 1) + fibaux(self, n - 2)
+
+        return fibaux(self, n)
